d1f9914edd8eab40157e61c61abefaf62571a223
georgivelikov-devsearch <gmvelikov1@gmail.com>
2022-03-12 12:55:11 +0200

    Initial commit

---
+++ devsearch-images-ws/src/main/java/devsearch/images/ws/DevsearchImagesWsApplication.java
@@ -0,0 +1,13 @@
+package devsearch.images.ws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevsearchImagesWsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevsearchImagesWsApplication.class, args);
+	}
+
+}
---
+++ devsearch-images-ws/src/test/java/devsearch/images/ws/DevsearchImagesWsApplicationTests.java
@@ -0,0 +1,13 @@
+package devsearch.images.ws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevsearchImagesWsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
